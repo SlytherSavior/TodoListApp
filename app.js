a78b9e1e5b1f1c4824461b4c7e80bfbec0fd675f
Slyther Savior <shrijanpoudel9@gmail.com>
2024-03-07 22:00:30 +0545

    feat: added modules and reduced LOC

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const date = require(__dirname + "/date.js");
 
-var userInputs = [];
+const workItems = [];
+const userInputs = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -11,25 +13,18 @@ app.set('view engine', 'ejs');
 
 app.get("/", (req,res) => { 
 
-  let today = new Date();
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: "long"
-  };
-
-  let date = today.toLocaleDateString("en-US", options);
+  const currentDate = date.getDate()
  
   
-  res.render("list", {kindOfDay: date, newListItem: userInputs});
+  res.render("list", {listTitle: currentDate, newListItem: userInputs});
 
 })
 
 app.post("/", (req,res) => { 
-   let userInput = req.body.newItem ; 
+   const userInput = req.body.newItem ; 
    userInputs.push(userInput);
    console.log("User added: " + userInput);
-  res.redirect("/");
+   res.redirect("/");
 })
 
 
@@ -38,3 +33,12 @@ app.listen(3000, () => {
   console.log("The server is up and running at port 3000"); 
 })
 
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItem: workItems});
+})
+
+app.post("/work", function(req,res){ 
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
---
+++ date.js
@@ -0,0 +1,12 @@
+exports.getDate = function() { 
+
+  let today = new Date();
+  let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+}
